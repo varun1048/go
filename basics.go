26d6cfa8920722d0e9284a14b4f5915dfb68dd82
varun <59528216+varun1048@users.noreply.github.com>
2020-10-31 14:28:24 +0530

    Update basics.go

--- basics.go
+++ basics.go
@@ -14,30 +14,40 @@ func main() {
 	num, num2, num3 := 9, 4, 6
 	i := 1
 	const con = 10
-	for i <= 4 {
-		out1, out2 := cal(con, i)
-		fmt.Println(out1, out2)
+	next := closure()
+		for i <= 4 {
+			out1, out2 := cal(con, i)
+			fmt.Println(out1, out2)
+			
+			fmt.Println(next())		
 		i++
-	}
-
+				
+		}
+	
+	
 	var fl float64 = 13
 	fmt.Printf("from math function: %.3f", math.Sqrt(fl))
 	fmt.Println("\n", num, num2, num3)
 
 	x, y := "thala", "thalapathy"
-	change(x, y)
+	change(&x, &y)
 	fmt.Println(x,y)
 
 }
-
-
-func change(x, y string) string {
+func closure() func() int {
+   i:=0
+   return func() int {
+      i+=1
+	  return i  
+   }
+}
+func change(x, y *string) {
 	var result string
 
-	result = x
-	x = y
-	y = result
+	result = *x
+	*x = *y
+	*y = result
 
-	return result
+	//return result
 
 }
